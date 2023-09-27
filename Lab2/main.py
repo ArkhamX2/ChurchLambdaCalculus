e755872623c476bf3447571a51ab62138d3a56f3
jowlly <pa.gromova@gmail.com>
2023-09-27 11:44:39 +0300

    lab2

---
+++ Lab2/main.py
@@ -0,0 +1,16 @@
+import lib
+from lib.person import Person
+
+
+Meowka = Person("Meow", "Meowickin", "1")
+Meowka.info()
+Meowka2 = Person("Meow2", "Meow", "1")
+Meowka2.info()
+Meowkaz = Person("Meowkaz", "Meowin", "1")
+Meowkaz.info()
+
+Meowka.faired()
+Meowkaz.up()
+Meowkaz.info()
+input()
+
