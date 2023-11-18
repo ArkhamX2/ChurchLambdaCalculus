34bedbf77189464dff70b387541cf0e095a9fab7
vadimyt <pvcs@yandex.ru>
2023-11-18 23:26:51 +0300

    more unit tests

--- ChurchLambdaCalculus/unittests/function_test.py
+++ ChurchLambdaCalculus/unittests/function_test.py
@@ -53,3 +53,48 @@ class Test(unittest.TestCase):
 
     def test_DIVIDE(self):
         self.assertEqual(encodeNatural(DIVIDE(decodeNatural(14))(decodeNatural(7))),2)
+
+    def test_IS_ZERO(self):
+        self.assertEqual(IS_ZERO(decodeNatural(0)),TRUE)
+    
+    def test_IS_GREATER_OR_EQUAL(self):
+        self.assertEqual(IS_GREATER_OR_EQUAL(decodeNatural(2))(decodeNatural(4)),FALSE)
+
+    def test_IS_GREATER(self):
+        self.assertEqual(IS_GREATER(decodeNatural(2))(decodeNatural(4)),FALSE)
+
+    def test_IS_LOWER_OR_EQUAL(self):
+        self.assertEqual(IS_LOWER_OR_EQUAL(decodeNatural(2))(decodeNatural(4)),TRUE)
+
+    def test_IS_LOWER(self):
+        self.assertEqual(IS_LOWER(decodeNatural(2))(decodeNatural(4)),TRUE)
+
+    def test_IS_EQUAL(self):
+        self.assertEqual(IS_EQUAL(decodeNatural(4))(decodeNatural(4)),TRUE)
+
+    def test_MIN(self):
+        self.assertEqual(encodeNatural(MIN(decodeNatural(2))(decodeNatural(4))),2)
+
+    def test_MAX(self):
+        self.assertEqual(encodeNatural(MAX(decodeNatural(2))(decodeNatural(4))),4)
+
+    def test_NEGATE(self):
+        self.assertEqual(encodeSigned(NEGATE(decodeSigned(-10))),10) 
+    
+    def test_IS_POSITIVE(self):
+        self.assertEqual(IS_POSITIVE(decodeSigned(-10)),FALSE) 
+    
+    def test_IS_NEGATIVE(self):
+        self.assertEqual(IS_NEGATIVE(decodeSigned(-10)),TRUE) 
+
+    def test_SIGNED_ADD(self):
+        self.assertEqual(encodeSigned(SIGNED_ADD(decodeSigned(-10))(decodeSigned(-5))),-15) 
+
+    def test_SIGNED_SUBTRACT(self):
+        self.assertEqual(encodeSigned(SIGNED_SUBTRACT(decodeSigned(-10))(decodeSigned(-5))),-5) 
+
+    def test_SIGNED_MULTIPLY(self):
+        self.assertEqual(encodeSigned(SIGNED_MULTIPLY(decodeSigned(-10))(decodeSigned(-5))),50) 
+
+    def test_SIGNED_DIVIDE(self):
+        self.assertEqual(encodeSigned(SIGNED_DIVIDE(decodeSigned(-10))(decodeSigned(-5))),2) 
