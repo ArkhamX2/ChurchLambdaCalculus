56202126c3de7217b8c8320c77e6ec38ad490f1a
vadimyt <pvcs@yandex.ru>
2023-11-19 21:06:03 +0300

    working EVALUATE

--- ChurchLambdaCalculus/UI/widget.py
+++ ChurchLambdaCalculus/UI/widget.py
@@ -1,12 +1,17 @@
 import sys
 
+import PySide6.QtGui
+
 # setting path
 sys.path.append('../library/')
 
 from typing import Optional
-from PySide6.QtCore import QtMsgType
+from PySide6 import QtCore
 from PySide6.QtWidgets import QWidget
+from PySide6.QtCore import QEvent
 from library.arithmetic_parser import Parse
+from library.arithmetic_evaluator import EvaluateEquation
+from library.function import *
 from UI.ui_calculus import Ui_MainWindow
 
 class Widget(QWidget,Ui_MainWindow):
@@ -14,6 +19,7 @@ class Widget(QWidget,Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.setWindowTitle("Lambda Calculus")
+        self.InputText=[]
         
         self.Plus.clicked.connect(self.plus)
         self.Minus.clicked.connect(self.minus)
@@ -38,54 +44,178 @@ class Widget(QWidget,Ui_MainWindow):
         self.ClearButton.clicked.connect(self.CLEARALL)
         self.DeleteButton.clicked.connect(self.DELETELAST)
         self.EqLog.clicked.connect(self.EQUALITY)
+        self.Evaluate.clicked.connect(self.EVALUATE)
+
+    def IsNumber(self, item):
+        match item:
+            case "0":
+                return True
+            case "1":
+                return True
+            case "2":
+                return True
+            case "3":
+                return True
+            case "4":
+                return True
+            case "5":
+                return True
+            case "6":
+                return True
+            case "7":
+                return True
+            case "8":
+                return True
+            case "9":
+                return True
+            case _:
+                return False
+
+    def MakeString(self):
+        answ=""
+        for item in self.InputText:
+            if self.IsNumber(item)==True:
+                answ+=item
+            else:
+                answ+=" "
+                answ+=item
+                answ+=" "
+        return answ
 
     def plus(self):
-        self.Input.setText(self.Input.toPlainText() + " +")
+        if len(self.InputText)==0:
+            self.InputText.append("0")
+            self.InputText.append("+")
+            self.Input.setText(self.Input.toPlainText() + "0" + self.InputText[len(self.InputText)-1])
+        else:
+            self.InputText.append("+")
+            self.Input.setText(self.Input.toPlainText() + self.InputText[len(self.InputText)-1])
     def minus(self):
-        self.Input.setText(self.Input.toPlainText() + " -")
+        if len(self.InputText)==0:
+            self.InputText.append("0")
+            self.InputText.append("-")
+            self.Input.setText(self.Input.toPlainText() + "0" + self.InputText[len(self.InputText)-1])
+        else:
+            self.InputText.append("-")
+            self.Input.setText(self.Input.toPlainText() + self.InputText[len(self.InputText)-1])
     def multiply(self):
-        self.Input.setText(self.Input.toPlainText() + " *")
+        if len(self.InputText)==0:
+            self.InputText.append("0")
+            self.InputText.append("*")
+            self.Input.setText(self.Input.toPlainText() + "0" + self.InputText[len(self.InputText)-1])
+        else:
+            self.InputText.append("*")
+            self.Input.setText(self.Input.toPlainText() + self.InputText[len(self.InputText)-1])
     def divide(self):
-        self.Input.setText(self.Input.toPlainText() + " /")
+        if len(self.InputText)==0:
+            self.InputText.append("0")
+            self.InputText.append("/")
+            self.Input.setText(self.Input.toPlainText() + "0" + self.InputText[len(self.InputText)-1])
+        else:
+            self.InputText.append("/")
+            self.Input.setText(self.Input.toPlainText() + self.InputText[len(self.InputText)-1])
     def OR(self):
-        self.Input.setText(self.Input.toPlainText() + " V")
+        self.InputText.append("V")
+        self.Input.setText(self.Input.toPlainText() + self.InputText[len(self.InputText)-1])
     def AND(self):
-        self.Input.setText(self.Input.toPlainText() + " ^")
+        self.InputText.append("^")
+        self.Input.setText(self.Input.toPlainText() + self.InputText[len(self.InputText)-1])
     def XOR(self):
-        self.Input.setText(self.Input.toPlainText() + " XOR")
+        self.InputText.append("XOR")
+        self.Input.setText(self.Input.toPlainText() + self.InputText[len(self.InputText)-1])
     def NOT(self):
-        self.Input.setText(self.Input.toPlainText() + " !")
+        self.InputText.append("!")
+        self.Input.setText(self.Input.toPlainText() + self.InputText[len(self.InputText)-1])
     def EQUALITY(self):
-        self.Input.setText(self.Input.toPlainText() + " ≡")
+        self.InputText.append("≡")
+        self.Input.setText(self.Input.toPlainText() + self.InputText[len(self.InputText)-1])
     def TRUE(self):
-        self.Input.setText(self.Input.toPlainText() + " True")
+        self.InputText.append("True")
+        self.Input.setText(self.Input.toPlainText() + self.InputText[len(self.InputText)-1])
     def FALSE(self):
-        self.Input.setText(self.Input.toPlainText() + " False")
+        self.InputText.append("False")
+        self.Input.setText(self.Input.toPlainText() + self.InputText[len(self.InputText)-1])
     def ONE(self):
-        self.Input.setText(self.Input.toPlainText() + "1")
+        self.InputText.append("1")
+        self.Input.setText(self.Input.toPlainText() + self.InputText[len(self.InputText)-1])
     def TWO(self):
-        self.Input.setText(self.Input.toPlainText() + "2")
+        self.InputText.append("2")
+        self.Input.setText(self.Input.toPlainText() + self.InputText[len(self.InputText)-1])
     def THREE(self):
-        self.Input.setText(self.Input.toPlainText() + "3")
+        self.InputText.append("3")
+        self.Input.setText(self.Input.toPlainText() + self.InputText[len(self.InputText)-1])
     def FOUR(self):
-        self.Input.setText(self.Input.toPlainText() + "4")
+        self.InputText.append("4")
+        self.Input.setText(self.Input.toPlainText() + self.InputText[len(self.InputText)-1])
     def FIVE(self):
-        self.Input.setText(self.Input.toPlainText() + "5")
+        self.InputText.append("5")
+        self.Input.setText(self.Input.toPlainText() + self.InputText[len(self.InputText)-1])
     def SIX(self):
-        self.Input.setText(self.Input.toPlainText() + "6")
+        self.InputText.append("6")
+        self.Input.setText(self.Input.toPlainText() + self.InputText[len(self.InputText)-1])
     def SEVEN(self):
-        self.Input.setText(self.Input.toPlainText() + "7")
+        self.InputText.append("7")
+        self.Input.setText(self.Input.toPlainText() + self.InputText[len(self.InputText)-1])
     def EIGHT(self):
-        self.Input.setText(self.Input.toPlainText() + "8")
+        self.InputText.append("8")
+        self.Input.setText(self.Input.toPlainText() + self.InputText[len(self.InputText)-1])
     def NINE(self):
-        self.Input.setText(self.Input.toPlainText() + "9")
+        self.InputText.append("9")
+        self.Input.setText(self.Input.toPlainText() + self.InputText[len(self.InputText)-1])
     def ZERO(self):
-        self.Input.setText(self.Input.toPlainText() + "0")
+        self.InputText.append("0")
+        self.Input.setText(self.Input.toPlainText() + self.InputText[len(self.InputText)-1])
     def CLEARALL(self):
+        self.InputText.clear()
         self.Input.setText("")
     def DELETELAST(self):
-        self.Input.setText(self.Input.toPlainText()[:len(self.Input.toPlainText())-1])
+        try:        
+            self.Input.setText(self.Input.toPlainText()[:len(self.Input.toPlainText())-len(self.InputText.pop())])
+        except:
+            pass
 
+    def EVALUATE(self):
+        if len(self.InputText)==0:
+            self.Result.setText("")
+        else:
+            self.Result.setText(str(eval(EvaluateEquation(Parse(self.MakeString())))))
 
-    def Evaluate(self):
-        self.Result.setText(Parse(self.Result.toPlainText()))
+    def keyPressEvent(self, event):
+        if (event.type() == QtCore.QEvent.KeyPress):
+                match event.key():
+                    case QtCore.Qt.Key_0:
+                        self.ZERO()
+                    case QtCore.Qt.Key_1:
+                        self.ONE()
+                    case QtCore.Qt.Key_2:
+                        self.TWO()
+                    case QtCore.Qt.Key_3:
+                        self.THREE()
+                    case QtCore.Qt.Key_4:
+                        self.FOUR()
+                    case QtCore.Qt.Key_5:
+                        self.FIVE()
+                    case QtCore.Qt.Key_6:
+                        self.SIX()
+                    case QtCore.Qt.Key_7:
+                        self.SEVEN()
+                    case QtCore.Qt.Key_8:
+                        self.EIGHT()
+                    case QtCore.Qt.Key_9:
+                        self.NINE()
+                    case QtCore.Qt.Key_Plus:
+                        self.plus()
+                    case QtCore.Qt.Key_Minus:
+                        self.minus()
+                    case QtCore.Qt.Key_Asterisk:
+                        self.multiply()
+                    case QtCore.Qt.Key_Slash:
+                        self.divide()
+                    case QtCore.Qt.Key_Backspace:
+                        self.DELETELAST()
+                    case QtCore.Qt.Key_Delete:
+                        self.CLEARALL()
+                    case QtCore.Qt.Key_Enter:
+                        self.EVALUATE()
+                    case QtCore.Qt.Key_Return:
+                        self.EVALUATE()
